b9d8bd5dd00e7af142f8676f6ac1d421ba6dcdb4
Fran Lara <francesc.lara@hotmail.com>
2021-06-16 18:52:42 +0200

    all js done css pending

--- js/script.js
+++ js/script.js
@@ -7,7 +7,10 @@ const threeLettersWords = [
     "you",
     "age",
     "two",
-    "can"
+    "can",
+    "any",
+    "new",
+    "old"
 ]
 
 const fourLettersWords = [
@@ -36,6 +39,31 @@ const fiveLettersWords = [
     "party"
 ]
 
+const sixLettersWords = [
+    "should",
+    "before",
+    "people",
+    "little",
+    "during",
+    "number",
+    "system",
+    "states",
+    "social"
+]
+
+const sevenLettersWords = [
+    "justify",
+    "example",
+    "project",
+    "citizen",
+    "subject",
+    "quality",
+    "exactly",
+    "complex",
+    "analyze",
+    "horizon"
+]
+
 const submitBtn = document.getElementById("submit");
 const resetBtn = document.getElementById("reset");
 const startBtn = document.getElementById("start");
@@ -51,6 +79,7 @@ const correctOrNot = document.getElementById('correctOrNotWord');
 let level = 1;
 let score = 0;
 let attempts = 0;
+let nextLevel = 0;
 let originalWord;
 
 // Function to start the game when we click on Start the Game
@@ -58,6 +87,16 @@ function start(){
     screenGame.classList.remove("hidden");
     this.style.display="none";
     }
+function resetGame(){
+    level = 1;
+    score = 0;
+    attempts = 0;
+    levelDisplay.innerText = level;
+    scoreDisplay.innerText = score;
+    attemptsDisplay.innerText = attempts;
+    gameLevel();
+    //wordRandom.innerText = "";
+}
 
 // Execute function start when click the btn
 playBtn.addEventListener('click',start);
@@ -73,61 +112,74 @@ function originalRandomWord(word){
 function scrambledWord(originalWord){
     let temp = originalWord.split("");
     temp.sort();
-    console.log(temp);
+    //console.log(temp);
     let scrambledFinalWord = temp.join("");
-    console.log(scrambledFinalWord);
+    //console.log(scrambledFinalWord);
     return scrambledFinalWord;
     }
 
 // Update display after reset or losing a game
+/*
 function displayReset(){
     levelDisplay = level;
     scoreDisplay = score;
     attemptsDisplay = attempts;
 }
-
+*/
 function displayUpdate(){
     levelDisplay.innerText = level;
     scoreDisplay.innerText = score;
-    attemptsDisplay.innerText = attempts;
+    //attemptsDisplay.innerText = attempts;
 }
 
 //Check word if it is correct or not
 function checkWord(){
     let tempWord = typeWord.value;
+    if(attempts == 2){
+        
+        resetGame();
+    }
     if(tempWord === originalWord){
     correctOrNot.innerText = "Correct";
     typeWord.value="";
     level += 1;
+    //console.log(level)
     score += 1;
     gameLevel();
     displayUpdate();
     }
+   
     else {
     correctOrNot.innerText = "Wrong"; 
     typeWord.value="";   
     attempts += 1;
-    displayUpdate();
+    attemptsDisplay.innerText = attempts;
     }
 }
 
 submitBtn.addEventListener('click',checkWord);
 
 function gameLevel() {
-    if (level == 1) {
+    if (level == 1 || level == 2) {
         originalRandomWord(threeLettersWords);
     }
-    else if (level == 2) {
+    else if (level == 3 |  level == 4) {
         originalRandomWord(fourLettersWords);
     } 
-    else if (level == 3) {
+    else if (level == 5 |  level == 6) {
         originalRandomWord(fiveLettersWords);
+    }
+    else if (level == 7 | level == 8){
+        originalRandomWord(sixLettersWords);
+    }
+    else if (level == 9 | level == 10){
+        originalRandomWord(sevenLettersWords);
     }
     wordRandom.innerText = scrambledWord(originalWord);
 
   }
-
   gameLevel();
 
 // DOM reset botton
-resetBtn.addEventListener('click',displayReset);
+resetBtn.addEventListener('click',resetGame);
+
